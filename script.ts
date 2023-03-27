6fb594b448929cedb4782f4b4863935850027435
mryhryki <mryhryki@gmail.com>
2023-03-27 13:50:20 +0900

    4.3. Explore relation queries with Prisma
    https://www.prisma.io/docs/getting-started/quickstart#43-explore-relation-queries-with-prisma

--- script.ts
+++ script.ts
@@ -11,8 +11,25 @@ async function main() {
   // })
   // console.log(user)
 
-  const users = await prisma.user.findMany()
-  console.log(users)
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Bob',
+  //     email: 'bob@prisma.io',
+  //     posts: {
+  //       create: {
+  //         title: 'Hello World',
+  //       },
+  //     },
+  //   },
+  // })
+  // console.log(user)
+
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
 }
 
 main()
